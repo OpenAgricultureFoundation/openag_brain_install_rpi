af786e5e183fcac05e18498ba217f85858d6f7ff
Leon Chambers <leon8556@gmail.com>
2016-06-17 14:26:27 +0000

    Initial script attempt

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# openag_brain_install_rpi
-A script for installing the openag_brain code on a Raspberry Pi
+openag\_brain\_install\_rpi
+---------------------------
+
+This repository for installing the openag\_brain project on a Raspberry Pi.
+Unless you are going to be modifying the
+[openag\_brain](https://github.com/OpenAgInitiative/openag_brain) code, you
+probably want to use the [Docker
+setup](https://github.com/OpenAgInitiative/openag_brain_docker_rpi) instead of
+this script.
+
+To set up the project, simply clone the repository and run the `install.sh`
+file.
+
+    git clone https://github.com/OpenAgInitiative/openag_brain_install_rpi.git
+    cd openag_brain_install_rpi
+    ./install.sh
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+# Tell apt to read from the ROS package repository
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu jessie main" > /etc/apt/sources.list.d/ros-latest.list'
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
+
+# Update apt
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Install dependencies
+sudo apt-get install python-pip
+sudo pip install rosdep rosinstall_generator wstool
+
+# Initialize rosdep
+sudo rosdep init
+rosdep update
+
+# Create a catkin workspace
+mkdir -p ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+
+# Install ros_comm and rosserial
+echo Generating rosinstall file...
+rm -f indigo.rosinstall src/.rosinstall
+rosinstall_generator ros_comm rosserial rosserial_arduino --rosdistro indigo --deps --wet-only --exclude roslisp --tar > indigo.rosinstall
+wstool init src indigo.rosinstall
+
+# Install dependencies for all installed packages
+rosdep install --from-paths src --ignore-src --rosdistro indigo -y -r --os=debian:jessie
+
+# Install packages in system directory
+sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/indigo -j2
+
+# Activate the ROS environment
+source /opt/ros/indigo/setup.bash
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+
+# Install couchdb
+sudo apt-get install -y couchdb
+
+# Create a catkin workspace to work in and install openag_brain in it
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+git clone https://github.com/OpenAgInitiative/openag_brain.git -b ros
+cd ~/catkin_ws
+catkin_make
+catkin_make install
+source devel/setup.bash
+rosdep install -i -y openag_brain || exit 1
+rosrun openag_brain install_pio
